48e4f9d229f3ebd5984a263d52c3e47fd648676a
Jordan <jordan.wenzel@nibiru-systems.com>
2024-11-05 11:31:14 +0100

    more mappings

--- src/python_deps_generator/cli.py
+++ src/python_deps_generator/cli.py
@@ -5,32 +5,170 @@ from pathlib import Path
 
 # Common package name mappings
 PACKAGE_MAPPINGS = {
-    # Data Science & ML
+    # Data Science & Machine Learning
     'sklearn': 'scikit-learn',
     'PIL': 'Pillow',
     'cv2': 'opencv-python',
-    'np': 'numpy',
+    'tensorflow.keras': 'tensorflow',
+    'keras': 'keras',
+    'nx': 'networkx',
     'pd': 'pandas',
+    'np': 'numpy',
     'plt': 'matplotlib',
+    'scipy.sparse': 'scipy',
+    'scipy.stats': 'scipy',
+    'scipy.optimize': 'scipy',
+    'scipy': 'scipy',
+    'skimage': 'scikit-image',
     'tf': 'tensorflow',
+    'torch.cuda': 'torch',
+    'torch.nn': 'torch',
+    'torch.utils': 'torch',
     'torch': 'torch',
+    'xgboost': 'xgboost',
+    'lightgbm': 'lightgbm',
     
-    # Web & APIs
+    # Web Development
     'bs4': 'beautifulsoup4',
     'flask_cors': 'Flask-Cors',
     'flask_sqlalchemy': 'Flask-SQLAlchemy',
     'flask': 'Flask',
     'jwt': 'PyJWT',
-    
-    # Utils & Config
-    'dotenv': 'python-dotenv',
-    'yaml': 'PyYAML',
-    'toml': 'toml',
+    'redis': 'redis-py',
+    'graphene': 'graphene-python',
+    'requests_oauthlib': 'requests-oauthlib',
     
     # Database
     'psycopg2': 'psycopg2-binary',
     'pymongo': 'pymongo',
     'sqlalchemy': 'SQLAlchemy',
+    
+    # Utils & Config
+    'dotenv': 'python-dotenv',
+    'yaml': 'pyyaml',
+    'ruamel.yaml': 'ruamel.yaml',
+    'toml': 'toml',
+    'conf': 'python-configuration',
+    
+    # CLI & System
+    'click': 'click',
+    'typer': 'typer',
+    'rich': 'rich',
+    'colorama': 'colorama',
+    'pypdf2': 'PyPDF2',
+    'pypdf': 'pypdf',
+    
+    # Testing
+    'pytest': 'pytest',
+    'nose': 'nose',
+    'mock': 'mock',
+    
+    # AWS
+    'boto3': 'boto3',
+    'botocore': 'botocore',
+    'awscli': 'awscli',
+    
+    # Azure
+    'azure.storage': 'azure-storage-blob',
+    'azure.cognitiveservices': 'azure-cognitiveservices-vision-computervision',
+    
+    # Google Cloud
+    'google.cloud': 'google-cloud',
+    'google.oauth2': 'google-auth',
+    
+    # Date & Time
+    'dateutil': 'python-dateutil',
+    'pytz': 'pytz',
+    'pendulum': 'pendulum',
+    
+    # Async
+    'aiohttp': 'aiohttp',
+    'asyncio': 'asyncio',
+    'fastapi': 'fastapi',
+    'uvicorn': 'uvicorn',
+    
+    # Image Processing
+    'imageio': 'imageio',
+    'wand': 'Wand',
+    
+    # Natural Language Processing
+    'nltk': 'nltk',
+    'spacy': 'spacy',
+    'gensim': 'gensim',
+    'transformers': 'transformers',
+    
+    # Documentation
+    'sphinx': 'Sphinx',
+    'mkdocs': 'mkdocs',
+    
+    # Serialization
+    'msgpack': 'msgpack-python',
+    'protobuf': 'protobuf',
+    
+    # Optimization
+    'pulp': 'PuLP',
+    'cvxopt': 'cvxopt',
+    
+    # Audio Processing
+    'librosa': 'librosa',
+    'soundfile': 'SoundFile',
+    
+    # Email
+    'email_validator': 'email-validator',
+    'smtplib': 'secure-smtplib',
+    
+    # Cryptography & Security
+    'bcrypt': 'bcrypt',
+    'cryptography': 'cryptography',
+    
+    # Progress Bars & CLI UI
+    'tqdm': 'tqdm',
+    'progressbar': 'progressbar2',
+    
+    # Code Quality & Formatting
+    'black': 'black',
+    'flake8': 'flake8',
+    'pylint': 'pylint',
+    'mypy': 'mypy',
+    
+    # Data Validation
+    'pydantic': 'pydantic',
+    'marshmallow': 'marshmallow',
+    'cerberus': 'Cerberus',
+    
+    # API Development
+    'connexion': 'connexion',
+    'swagger_ui': 'swagger-ui-bundle',
+    
+    # Templating
+    'jinja2': 'Jinja2',
+    'mako': 'Mako',
+    
+    # Process & System
+    'psutil': 'psutil',
+    'watchdog': 'watchdog',
+    
+    # Scientific
+    'sympy': 'sympy',
+    'statsmodels': 'statsmodels',
+    
+    # Caching
+    'cachetools': 'cachetools',
+    'memcache': 'python-memcached',
+    
+    # Compression
+    'zipfile': 'zipfile36',
+    'gzip': 'gzip-reader',
+    
+    # GUI
+    'tkinter': 'tk',
+    'qt': 'PyQt5',
+    'wx': 'wxPython',
+    
+    # Parsing
+    'lxml': 'lxml',
+    'feedparser': 'feedparser',
+    'html5lib': 'html5lib',
 }
 
 class ImportFinder(ast.NodeVisitor):
